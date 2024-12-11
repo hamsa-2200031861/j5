b812df308ce4b80981a30173a5579682e0d0c44c
hamsa-2200031861 <dandalingareddy028@gmail.com>
2024-12-11 21:39:53 +0530

    Describe the changes here

--- src/main/java/repository/EmployerRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import model.Employer;
-
-public interface EmployerRepository extends JpaRepository<Employer, Long> {
-    Employer findByEmailAndPassword(String email, String password);
-}
--- src/main/java/repository/PlacementOfficerRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import model.PlacementOfficer;
-
-public interface PlacementOfficerRepository extends JpaRepository<PlacementOfficer, Long> {
-    PlacementOfficer findByEmailAndPassword(String email, String password);
-}
